b96d05a93729635020f317ffc2f0343fc1be8be1
Lyda <1829913225@qq.com>
2026-03-03 18:38:27 +0800

    feat(core): 新增 Git SDK 本地变更管理能力
    为 GitSdkService 新增获取暂存区、工作区及未跟踪文件变更的方法，支持本地代码审查模式。新增方法包括：getStagedFiles、getUnstagedFiles、getUntrackedFiles、getUncommittedFiles、getWorkingFileContent 等，并导出 LocalReviewMode 类型。

--- packages/core/src/shared/git-sdk/index.ts
+++ packages/core/src/shared/git-sdk/index.ts
@@ -1,3 +1,4 @@
 export * from "./git-sdk.types";
 export * from "./git-sdk-diff.utils";
 export { GitSdkService } from "./git-sdk.service";
+export type { LocalReviewMode } from "./git-sdk.service";
